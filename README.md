d45828bd2ffa3b625a9270edf74f9f2c36a255c2
Reda <redazitouni245@gmail.com>
2022-11-04 02:48:50 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Wordle-Solver
-Python script to get the answers for wordle 
+A python solver for [Wordle](https://www.nytimes.com/games/wordle/index.html).
+
+## Requirements
+  * [Python 3.4+](https://www.python.org/downloads/)
+  * requests
+
+## Usage
+  * Download the source code.
+  * Run the "main.py" file.
+
+## Constraints
+  * The number of days is set to never go beyond 5.
+
+### Example usage
+```
+Enter the number days you want to get the answers for : 3
++------------+----------+
+|Date        |Solution  |
++============+==========+
+|2022-11-04  |photo     |
+|2022-11-05  |dream     |
+|2022-11-06  |stale     |
++------------+----------+
+```
---
+++ wordle.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import *
+
+baseLink = "https://www.nytimes.com/svc/wordle/v2/{}.json"
+solutions = {}
+
+def printTable(solutions):
+    maxLenDates = len(max(list(solutions.keys()) + ["Date"], key=len)) + 2
+    maxLenSolutions = len(max(list(solutions.values()) + ["Solution"], key=len)) + 2
+
+    print('+' + '-' * maxLenDates + '+' + '-' * maxLenSolutions + '+')
+    print('|' + "Date" + ' ' * (maxLenDates - len("Date")) + '|' + "Solution" + ' ' * (maxLenSolutions - len("Solution")) + '|')
+    print('+' + '=' * maxLenDates + '+' + '=' * maxLenSolutions + '+')
+    for date, solution in solutions.items():
+        print('|' + date + ' ' * (maxLenDates - len(date)) + '|' + solution + ' ' * (maxLenSolutions - len(solution)) + '|')
+    print('+' + '-' * maxLenDates + '+' + '-' * maxLenSolutions + '+')
+
+def main():
+    numberOfDays = int(input("Enter the number days you want to get the answers for : "))
+    numberOfDays = min(numberOfDays, 5)
+
+    for i in range(numberOfDays):
+        day = (date.today() + timedelta(days = i)).strftime("%Y-%m-%d")
+        link = baseLink.format(day)
+        res = requests.get(link).json()
+
+        solutions[res["print_date"]] = res["solution"]
+
+    printTable(solutions)
+
+if __name__ == "__main__":
+    main()
